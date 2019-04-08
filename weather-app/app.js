4a22fe2539feb7910f0fa9a8d2c5b60fac4c8a8a
Robin Tram <robin.tram@gmail.com>
2019-04-08 14:43:22 -0400

    DarkSky Challenge.

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,11 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/96c7cbd898b93f8353ed032aac2ac45a/37.8267,-122.4233'
 
-request({ url:url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    const data = response.body.currently
+
+    const temperature = data.temperature
+    const precipitation = data.precipProbability
+
+    console.log(`It is currently ${temperature} with a ${precipitation}% chance of rain.`)
 })
