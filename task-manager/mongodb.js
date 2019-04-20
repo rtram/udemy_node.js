683e3830f69f96916448360362865d2bd86c4568
Robin Tram <robin.tram@gmail.com>
2019-04-20 14:36:02 -0400

    Connect and Add User Collection to NOSQL

---
+++ task-manager/mongodb.js
@@ -0,0 +1,19 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to Connect to Database')
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('user').insertOne({
+        name: 'Robin',
+        age: 27
+    }) 
+
+})
