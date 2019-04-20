8a0a906a96d63d5938da05a45adda865290dd10c
Robin Tram <robin.tram@gmail.com>
2019-04-20 14:45:12 -0400

    Rename Users Collection in NoSQL and Create Tasks Collection

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -11,9 +11,52 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('user').insertOne({
-        name: 'Robin',
-        age: 27
-    }) 
+    // db.collection('user').insertOne({
+    //     name: 'Robin',
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
 
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Clean House',
+            completed: false
+        },
+        {
+            description: 'Vaccuum',
+            completed: false
+        },
+        {
+            description: 'Wash Dishes',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            console.log('Unable to insert')
+        }
+
+        console.log(result.ops)
+    })
 })
