8bc889b9541676534393e16501566e32671e2cf2
Robin Tram <robin.tram@gmail.com>
2019-04-21 20:44:30 -0400

    Update Many Challenge

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -10,7 +10,31 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        console.log(tasks)
+    // db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+    //     console.log(tasks)
+    // })
+
+    // db.collection('users').updateOne({
+    //      _id: new ObjectID("5cbb67c52d82833f2c1485b6") 
+    // }, {
+    //     $set: {
+    //         name: 'Robin'
+    //     }
+    // }).then(result => {
+    //     console.log(result.message)
+    // }).catch(error => {
+    //     console.log(error)
+    // })
+
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed:true
+        }
+    }).then(result => {
+        console.log(result)
+    }).catch(error => {
+        console.log(error)
     })
 })
